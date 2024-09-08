6eff0d5f8e2f1f573e4065bc337040da6d678a42
patch3 <sowillmove1@gmail.com>
2024-09-08 23:11:07 +0300

    add UserDetails

---
+++ src/main/java/ru/sudrf/ais/accounting/equipment/configs/constants/Role.java
@@ -0,0 +1,5 @@
+package ru.sudrf.ais.accounting.equipment.configs.constants;
+
+public final class Role {
+    public static final String USER = "USER";
+}
--- src/main/java/ru/sudrf/ais/accounting/equipment/security/UserDetailsImpl.java
+++ src/main/java/ru/sudrf/ais/accounting/equipment/security/UserDetailsImpl.java
@@ -1,26 +1,28 @@
 package ru.sudrf.ais.accounting.equipment.security;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import ru.sudrf.ais.accounting.equipment.configs.constants.Role;
 import ru.sudrf.ais.accounting.equipment.models.Client;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public record UserDetailsImpl(Client client) implements UserDetails {
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
-
-    @Override
-    public String getPassword() {
-        return "";
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + Role.USER));
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return client.getLogin();
+    }
+
+    @Override
+    public String getPassword() {
+        return client.getPasswordHash();
     }
 }
